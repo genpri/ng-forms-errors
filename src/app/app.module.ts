74a77325ea0d545400d5d1359c777f23bd60eb26
genpri <genci.prifti@gmail.com>
2020-11-03 16:14:52 +0300

    fix comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ControlErrorsDirective } from './control-errors.directive';
-import { ControlErrorComponent } from './control-error/control-error.component';
-import { ControlErrorContainerDirective } from './control-error-container.directive';
-import { FormSubmitDirective } from './form-submit.directive';
+import { AppComponent } from "./app.component";
+import { ControlErrorsDirective } from "./control-errors.directive";
+import { ControlErrorComponent } from "./control-error/control-error.component";
+import { ControlErrorContainerDirective } from "./control-error-container.directive";
+import { FormSubmitDirective } from "./form-submit.directive";
 
 @NgModule({
   imports: [BrowserModule, ReactiveFormsModule],
-  declarations: [AppComponent, HelloComponent, ControlErrorsDirective, ControlErrorComponent, ControlErrorContainerDirective, FormSubmitDirective],
+  declarations: [
+    AppComponent,
+    ControlErrorsDirective,
+    ControlErrorComponent,
+    ControlErrorContainerDirective,
+    FormSubmitDirective
+  ],
   entryComponents: [ControlErrorComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
